15b84c8af2a6c9ce4362ff7397970c6bcf5d0bd7
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-06-25 09:31:18 -0700

    corrected display data function, added functionality to ask the user what product and how much of it they would like to purchase, and dynamically update the database reducing the available quantity after each purchase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,11 +23,71 @@ connection.connect(err => {
 });
 
 function afterConnection() {
-  connection.query("SELECT * FROM products", (err, data) => {
+  connection.query("SELECT item_id, product_name, price FROM products", (err, data) => {
     if (err) throw err;
     // prints the results in an array
     console.table(data);
-   
-    connection.end();
+    askQuestions(data);
+    // connection.end();
+  });
+}
+
+function askQuestions(inventory) {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What product ID would you like to buy?",
+      name: "productID"
+    }
+  ]).then(function (answer) {
+    console.log(answer);
+    var id = parseInt(answer.productID);
+    var product = checkforId(id, inventory);
+    if (product) {
+      askForQuantity(product);
+
+    }
+    else {
+      console.log("Please select the right ID");
+      afterConnection();
+    }
+
+  });
+}
+
+function checkforId(id, inventory) {
+  for (var i = 0; i < inventory.length; i++) {
+    if (inventory[i].item_id == id) {
+      return inventory[i];
+    }
+  }
+}
+
+function askForQuantity(product) {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What quantity would you like to buy?",
+      name: "quantity"
+    }
+  ]).then(function (answer) {
+    var quantityEntered = parseInt(answer.quantity);
+    if (quantityEntered > product.stock_quantity) {
+      console.log("insufficient quantity");
+      afterConnection();
+    }
+    else {
+      makePurchase(product, quantityEntered);
+    }
+  })
+}
+
+function makePurchase(product, quantityEntered) {
+  connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?", [
+    quantityEntered, product.item_id
+  ], function (err, response) {
+    if (err) throw err
+    console.log("You have successfully purchased " + quantityEntered + " " + product.product_name);
+    afterConnection();
   });
 }
